a4920fce005890dd001a5dc4ddec37e556474d0c
Ladislav Dobrovič <dobrovic8@uniba.sk>
2022-12-13 19:20:35 +0100

    Update Player

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,13 @@
 import DataType.Card;
 import DataType.PlayerState;
+import DataType.Position.AwokenQueenPosition;
 import DataType.Position.HandPosition;
 import DataType.Position.Position;
+import DataType.Position.SleepingQueenPosition;
 import DataType.Queen;
 import Queens.AwokenQueens;
 import Queens.SleepingQueens;
+import Enumeration.CardType;
 
 import java.util.*;
 
@@ -15,6 +18,7 @@ public class Player {
     private AwokenQueens awokenQueens;
     private SleepingQueens sleepingQueens;
     private EvaluateAttack evaluateAttack;
+    private MoveQueen moveQueen;
 
 
     public Player(int playerIdx, Hand hand) {
@@ -22,6 +26,22 @@ public class Player {
         this.hand = hand;
         awokenQueens = new AwokenQueens(playerIdx);
         uptadePlayerState();
+    }
+
+    public int getPlayerIdx() {
+        return playerIdx;
+    }
+
+    public AwokenQueens getAwokenQueens() {
+        return awokenQueens;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public PlayerState getPlayerState() {
+        return playerState;
     }
 
     public void uptadePlayerState() {
@@ -43,43 +63,124 @@ public class Player {
         playerState = new PlayerState(cards, queens);
     }
 
-    public int getPlayerIdx() {
-        return playerIdx;
+    public void setEvaluateAttack(EvaluateAttack evaluateAttack) {
+        this.evaluateAttack = evaluateAttack;
     }
 
-    public AwokenQueens getAwokenQueens() {
-        return awokenQueens;
-    }
-
-    public Hand getHand() {
-        return hand;
-    }
-
-    public PlayerState getPlayerState() {
-        return playerState;
+    public void setMoveQueen(MoveQueen moveQueen) {
+        this.moveQueen = moveQueen;
     }
 
     public boolean play(List<Position> cards) {
-        if ((cards.isEmpty()) || !(cards.get(0) instanceof HandPosition)) {
+        if ((cards.isEmpty()) || !(cards.get(0) instanceof HandPosition) || (((HandPosition) cards.get(0)).getPlayerIndex() != playerIdx)) {
             return false;
         }
 
         List<HandPosition> handPositions = new ArrayList<>();
+
         if (cards.size() == 1) {
-            handPositions.add((HandPosition) cards.get(0));
-        } else if (cards.size() == 2) {
-            Optional<Card> firstCard = playerState.getCards().get(cards.get(0).getCardIndex());
-            if (firstCard.isEmpty()) {
+            if (hand.getCards().get(cards.get(0).getCardIndex()).getType() != CardType.Number) {
                 return false;
+            } else {
+                handPositions.add((HandPosition) cards.get(0));
+            }
+        } else if (cards.size() == 2) {
+            Card firstCard = hand.getCards().get(cards.get(0).getCardIndex());
+
+            Position targetQueen = cards.get(1);
+            switch (firstCard.getType()) {
+                case King:
+                    if (targetQueen instanceof SleepingQueenPosition) {
+                        moveQueen.setQueenCollection(awokenQueens);
+                        if (!moveQueen.play(targetQueen)) {
+                            return false;
+                        } else {
+                            handPositions.add((HandPosition) cards.get(0));
+                        }
+                    } else {
+                        return false;
+                    }
+                    break;
+                case Knight:
+                    if (targetQueen instanceof AwokenQueenPosition) {
+                        if (((AwokenQueenPosition) targetQueen).getPlayerIndex() == playerIdx) {
+                            return false;
+                        }
+                        evaluateAttack.setDefenseCardType(CardType.Dragon);
+                        evaluateAttack.setQueenCollection(awokenQueens);
+                        if (!evaluateAttack.play(targetQueen, ((AwokenQueenPosition) targetQueen).getPlayerIndex())) {
+                            return false;
+                        }
+                        handPositions.add((HandPosition) cards.get(0));
+                    } else {
+                        return false;
+                    }
+                    break;
+                case SleepingPotion:
+                    if (targetQueen instanceof AwokenQueenPosition) {
+                        if (((AwokenQueenPosition) targetQueen).getPlayerIndex() == playerIdx){
+                            return false;
+                        }
+                        evaluateAttack.setDefenseCardType(CardType.MagicWand);
+                        evaluateAttack.setQueenCollection(sleepingQueens);
+                        if (!evaluateAttack.play(targetQueen, ((AwokenQueenPosition) targetQueen).getPlayerIndex())) {
+                            return false;
+                        }
+                        handPositions.add((HandPosition) cards.get(0));
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+                    if (!(cards.get(1) instanceof HandPosition)) {
+                        return false;
+                    }
+
+                    Card secondCard = hand.getCards().get(cards.get(1).getCardIndex());
+                    if ((firstCard.getType() != CardType.Number) || (secondCard.getType() != CardType.Number)) {
+                        return false;
+                    } else {
+                        EvaluateNumberedCards evaluateNumberedCards = new EvaluateNumberedCards();
+                        List<Card> eval = new ArrayList<>();
+                        eval.add(firstCard);
+                        eval.add(secondCard);
+                        if (!evaluateNumberedCards.play(eval)) {
+                            return false;
+                        }
+                        handPositions.add((HandPosition) cards.get(0));
+                        handPositions.add((HandPosition) cards.get(1));
+                    }
+            }
+        } else {
+            ArrayList<Card> numberCards = new ArrayList<>();
+            ArrayList<Integer> tempCardPositions = new ArrayList<>();
+
+            for (Position position : cards) {
+                if (!(position instanceof HandPosition)  || (((HandPosition) position).getPlayerIndex() != playerIdx)) {
+                    return false;
+                }
+                Card card = hand.getCards().get(cards.get(0).getCardIndex());
+
+                if(!tempCardPositions.contains(position.getCardIndex())) {
+                    tempCardPositions.add(position.getCardIndex());
+                    if(card.getType() != CardType.Number){
+                        return false;
+                    } else {
+                        numberCards.add(card);
+                        handPositions.add((HandPosition) position);
+                    }
+                }
             }
 
-            switch (firstCard.get().getType()) {
-                case King:
-
-                    break;
+            EvaluateNumberedCards evaluateNumberedCards = new EvaluateNumberedCards();
+            if (!evaluateNumberedCards.play(numberCards)) {
+                return false;
             }
         }
 
+        hand.pickCards(handPositions);
+        hand.removePickedCardsAndRedraw();
+        uptadePlayerState();
         return true;
     }
 }
